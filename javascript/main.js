c02d1c8ea4a6fee2e95dc6a7af5d99899e22cd53
jgthms <bbxdesign@gmail.com>
2016-06-11 15:56:48 +0100

    JS init

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,34 @@
 jQuery(document).ready(function ($) {
 
+  var now = moment();
+  var $dates = $('.date');
   var $games = $('.game');
-  console.log($games.length);
+
+  $dates.each(function() {
+    var $date = $(this);
+    var start = moment($date.attr('datetime'));
+
+    if (moment(now).isSame(start, 'day')) {
+      $date.addClass('is-today');
+      $('html, body').animate({
+        scrollTop: $date.offset().top
+      }, 1);
+    }
+  });
+
+  $games.each(function() {
+    var $game = $(this);
+    var start = moment($game.attr('datetime'));
+    var end = moment(start).add(105, 'm');
+    var $time = $game.children('.time');
+
+    $time.text(moment(start).format('HH:mm'));
+    $time.css('visibility', 'visible');
+
+    if (moment(now).isBetween(start, end)) {
+      $game.addClass('is-now');
+      $time.text('Now');
+    }
+  });
 
 });
